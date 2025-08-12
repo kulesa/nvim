1532273fc8dddce542204f21b522b75551499062
Aleksey Kuleshov <kulesa@gmail.com>
2025-08-12 16:20:42 +0300

    initial commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("onedark").setup({
+        style = "darker",
+      })
+      -- Enable theme
+      --require("onedark").load()
+    end,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    init = function()
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        pattern = "kanagawa",
+        callback = function()
+          vim.api.nvim_set_hl(0, "StatusLine", { link = "lualine_c_normal" })
+        end,
+      })
+    end,
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa",
+    },
+  },
+}
