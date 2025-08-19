d7fc6a4d9128e79c43f7a569ea03a8467a43a150
Aleksey Kuleshov <kulesa@gmail.com>
2025-08-19 12:32:50 +0300

    vscode colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,13 @@
 return {
   {
-    "navarasu/onedark.nvim",
+    "Mofiqul/vscode.nvim",
+    lazy = false,
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      require("onedark").setup({
-        style = "darker",
+      require("vscode").setup({
+        transparent = true,
+        italic_comments = false,
       })
-      -- Enable theme
-      --require("onedark").load()
     end,
   },
   {
@@ -26,7 +26,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa",
+      colorscheme = "vscode",
     },
   },
 }
